f4092273a9200ae487162deedddd970f107aac0d
somini <somini29@yandex.com>
2014-09-28 03:04:34 +0100

    Adds Makefile
    This makefile will regenerate a meta.js file, so that the script can be automatically updated.

---
+++ redirector.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        The Redirector v4
+// @namespace   somini
+// @include     *
+// @version     4.2.5
+// @require		https://github.com/derek-watson/jsUri/raw/master/Uri.js
+// @grant       none
+// @run-at		document-start
+// @description	Redirects from mobile sites
+// @description	Removes tracking UTM_*
+// @description	Cleans up some URLs, like NYT, YouTube, Cracked and more
+// ==/UserScript==
